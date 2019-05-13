07b244b9208faeb4b4e39eedf33f0b170864f58f
IamTao <itamtao@gmail.com>
2019-05-13 23:24:06 +0200

    have a dirty but working setup/

---
+++ ansible/roles/user-lin/files/compile.sh
@@ -0,0 +1,5 @@
+cd ~/install/pytorch
+
+TORCH_CUDA_ARCH_LIST="3.5 3.7 5.2 6.0 6.1 7.0+PTX" TORCH_NVCC_FLAGS="-Xfatbin -compress-all" \
+    CMAKE_PREFIX_PATH="$(dirname $(which conda))/../" \
+    pip install -v .
